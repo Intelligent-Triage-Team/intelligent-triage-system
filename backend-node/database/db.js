176f0c598b8cf3e3d159494575c190f6314baea3
Abebe Nega <your_email@example.com>
2026-05-19 00:40:58 +0300

    Remove env from tracking

--- backend-node/database/db.js
+++ backend-node/database/db.js
@@ -1,15 +1,13 @@
+require("dotenv").config();
+
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "abebe@hu",
-  database: "healthcare_db",
-
-  // ✅ Ethiopia timezone
-  timezone: "+03:00"
-  // timezone: "+00:00"
-  
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  timezone: process.env.DB_TIMEZONE
 });
 
 db.connect((err) => {
